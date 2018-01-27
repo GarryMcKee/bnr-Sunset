9c3da09118d7507852a051f9dba076335cb1062b
GarryMcKee <gmk0232@gmail.com>
2018-01-27 13:58:55 +0000

    Initial Commit (Finished Animation)

---
+++ app/src/main/java/com/mellobit/garrymckee/sunset/SunsetActivity.java
@@ -0,0 +1,19 @@
+package com.mellobit.garrymckee.sunset;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SunsetActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+    }
+}
