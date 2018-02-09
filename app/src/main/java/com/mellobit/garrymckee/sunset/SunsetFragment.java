6a30b65232944bb9bfd6721b20b6a1231c27b491
GarryMcKee <gmk0232@gmail.com>
2018-02-09 18:18:47 +0000

    Allow reversal of sunrise

--- app/src/main/java/com/mellobit/garrymckee/sunset/SunsetFragment.java
+++ app/src/main/java/com/mellobit/garrymckee/sunset/SunsetFragment.java
@@ -6,6 +6,8 @@ import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.res.Resources;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -33,9 +35,14 @@ public class SunsetFragment extends Fragment implements ViewTreeObserver.OnGloba
     private int mNightSkyColor;
 
     float mSunTopPosition;
-    float mReflectionBottomPosition;
 
     private boolean isSunSet;
+    private boolean isAnimating;
+
+    private int elapsedTime;
+
+    private AnimatorSet mAnimatorSet;
+
     private static final int ANIMATION_TIME = 2500;
 
     public static SunsetFragment newInstance() {
@@ -61,10 +68,61 @@ public class SunsetFragment extends Fragment implements ViewTreeObserver.OnGloba
         mSceneView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!isSunSet){
-                    startSunSetAnimation();
+                if(isAnimating) {
+                    Log.d("AnimCheck", "Attempting to cancel...");
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        elapsedTime = (int)mAnimatorSet.getCurrentPlayTime();
+                    } else {
+                        elapsedTime = ANIMATION_TIME;
+                    }
+                    mAnimatorSet.cancel();
+                    reverseAnimation();
+                    isAnimating = false;
                 } else {
-                    startSunRiseAnimation();
+                    if(!isSunSet){
+                        startSunSetAnimation();
+                    } else {
+                        startSunRiseAnimation();
+                    }
+                }
+
+
+            }
+
+            private void reverseAnimation() {
+                if(!isSunSet) {
+
+                } else {
+                    float sunYStart = mSunView.getY();
+                    float reflectionYStart = mReflectionView.getY();
+                    int skyColorStart = ((ColorDrawable) mSkyView.getBackground()).getColor();
+
+                    ObjectAnimator heightAnimator = ObjectAnimator
+                            .ofFloat(mSunView, "y", sunYStart, mSunTopPosition)
+                            .setDuration(elapsedTime);
+
+                    ObjectAnimator reflectionHeightAnimator = ObjectAnimator
+                            .ofFloat(mReflectionView, "y", reflectionYStart, mSkyView.getHeight() - (mSunTopPosition + mSunView.getHeight()) )
+                            .setDuration(elapsedTime);
+
+                    ObjectAnimator sunSkyAnimator = ObjectAnimator
+                            .ofInt(mSkyView, "backgroundColor", skyColorStart, mBlueSkyColor)
+                            .setDuration(elapsedTime);
+
+                    mAnimatorSet = new AnimatorSet();
+
+                    sunSkyAnimator.setEvaluator(new ArgbEvaluator());
+                    heightAnimator.setInterpolator(new AccelerateInterpolator());
+                    reflectionHeightAnimator.setInterpolator(new AccelerateInterpolator());
+
+                    mAnimatorSet
+                            .play(heightAnimator)
+                            .with(reflectionHeightAnimator)
+                            .with(sunSkyAnimator);
+
+                    mAnimatorSet.start();
+
+                    isAnimating = true;
                 }
             }
         });
@@ -116,15 +174,25 @@ public class SunsetFragment extends Fragment implements ViewTreeObserver.OnGloba
         checkDistance();
 
         float sunYStart = mSunView.getY();
-
         float sunYEnd = mSkyView.getHeight();
+        float reflectionYStart = mReflectionView.getY();
+        float reflectionYEnd = 0 - mReflectionView.getHeight();
+
+        AnimationTarget target = new AnimationTarget();
+
+        target.setSunStart(sunYStart);
+        target.setSunEnd(sunYEnd);
+        target.setReflectionStart(reflectionYStart);
+        target.setReflectionEnd(reflectionYEnd);
+        target.setColorStart(mBlueSkyColor);
+        target.setColorEnd(mNightSkyColor);
 
         ObjectAnimator heightAnimator = ObjectAnimator
                 .ofFloat(mSunView, "y", sunYStart, sunYEnd)
                 .setDuration(ANIMATION_TIME);
 
         ObjectAnimator reflectionHeightAnimator = ObjectAnimator
-                .ofFloat(mReflectionView, "y", mReflectionView.getY(), 0 - mReflectionView.getHeight())
+                .ofFloat(mReflectionView, "y", reflectionYStart, reflectionYEnd)
                 .setDuration(ANIMATION_TIME);
 
         ObjectAnimator sunSkyAnimator = ObjectAnimator
@@ -140,17 +208,19 @@ public class SunsetFragment extends Fragment implements ViewTreeObserver.OnGloba
         heightAnimator.setInterpolator(new AccelerateInterpolator());
         reflectionHeightAnimator.setInterpolator(new AccelerateInterpolator());
 
-        AnimatorSet sunSetAnimatorSet = new AnimatorSet();
-        sunSetAnimatorSet
+        mAnimatorSet = new AnimatorSet();
+        mAnimatorSet
                 .play(heightAnimator)
                 .with(reflectionHeightAnimator)
                 .with(sunSkyAnimator)
                 .before(nightSkyAnimator);
-        sunSetAnimatorSet.start();
+        mAnimatorSet.start();
+        isAnimating = true;
 
-        sunSetAnimatorSet.addListener(new Animator.AnimatorListener() {
+        mAnimatorSet.addListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animator) {
+                Log.i("TEST", "Animation Starting");
 
             }
 
@@ -158,6 +228,8 @@ public class SunsetFragment extends Fragment implements ViewTreeObserver.OnGloba
             public void onAnimationEnd(Animator animator) {
                 Log.i("TEST", "Animation Finished");
                 isSunSet = true;
+                isAnimating = false;
+                mAnimatorSet = null;
             }
 
             @Override
@@ -175,7 +247,19 @@ public class SunsetFragment extends Fragment implements ViewTreeObserver.OnGloba
     private void startSunRiseAnimation() {
         float sunYStart = mSeaView.getTop();
         float sunYEnd = mSunTopPosition;
+
+        float reflectionYStart = 0 - mReflectionView.getHeight();
+        float reflectionYEnd = mSkyView.getHeight() - (mSunTopPosition + mSunView.getHeight());
         Log.d("SUNCOORDS", "Sun to: " + sunYEnd);
+
+        AnimationTarget target = new AnimationTarget();
+
+        target.setSunStart(sunYStart);
+        target.setSunEnd(sunYEnd);
+        target.setReflectionStart(reflectionYStart);
+        target.setReflectionEnd(reflectionYEnd);
+        target.setColorStart(mNightSkyColor);
+        target.setColorEnd(mBlueSkyColor);
 
         ObjectAnimator heightAnimator = ObjectAnimator
                 .ofFloat(mSunView, "y", sunYStart, sunYEnd)
@@ -186,7 +270,7 @@ public class SunsetFragment extends Fragment implements ViewTreeObserver.OnGloba
                 .setDuration(ANIMATION_TIME);
 
         ObjectAnimator reflectionHeightAnimator = ObjectAnimator
-                .ofFloat(mReflectionView, "y", 0 - mReflectionView.getHeight(), (mSkyView.getHeight() - (mSunTopPosition + mSunView.getHeight())))
+                .ofFloat(mReflectionView, "y", reflectionYStart, reflectionYEnd)
                 .setDuration(ANIMATION_TIME);
 
         ObjectAnimator nightSkyAnimator = ObjectAnimator
@@ -199,29 +283,29 @@ public class SunsetFragment extends Fragment implements ViewTreeObserver.OnGloba
         heightAnimator.setInterpolator(new AccelerateInterpolator());
         reflectionHeightAnimator.setInterpolator(new AccelerateInterpolator());
 
-        AnimatorSet sunRiseAnimatorSet = new AnimatorSet();
-        sunRiseAnimatorSet
+        mAnimatorSet = new AnimatorSet();
+        mAnimatorSet
                 .play(heightAnimator)
                 .with(reflectionHeightAnimator)
                 .with(sunSkyAnimator)
                 .after(nightSkyAnimator);
-        sunRiseAnimatorSet.start();
+        mAnimatorSet.start();
 
-        sunRiseAnimatorSet.addListener(new Animator.AnimatorListener() {
+        mAnimatorSet.addListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animator) {
-
+                isAnimating = true;
             }
 
             @Override
             public void onAnimationEnd(Animator animator) {
                 Log.i("TEST", "Animation Finished");
                 isSunSet = false;
+                isAnimating = false;
             }
 
             @Override
             public void onAnimationCancel(Animator animator) {
-
             }
 
             @Override
@@ -240,5 +324,112 @@ public class SunsetFragment extends Fragment implements ViewTreeObserver.OnGloba
 
         Log.d("DISTCHECK", "Top Distance: " + topDistance);
         Log.d("DISTCHECK", "Bottom Distance: " + bottomDistance);
+    }
+
+    class AnimationTarget {
+        private float sunStart;
+        private float sunEnd;
+        private float reflectionStart;
+        private float reflectionEnd;
+
+        private int colorStart;
+        private int colorEnd;
+
+        public void setSunStart(float sunStart) {
+            this.sunStart = sunStart;
+        }
+
+        public void setSunEnd(float sunEnd) {
+            this.sunEnd = sunEnd;
+        }
+
+        public void setReflectionStart(float reflectionStart) {
+            this.reflectionStart = reflectionStart;
+        }
+
+        public void setReflectionEnd(float reflectionEnd) {
+            this.reflectionEnd = reflectionEnd;
+        }
+
+        public void setColorStart(int colorStart) {
+            this.colorStart = colorStart;
+        }
+
+        public void setColorEnd(int colorEnd) {
+            this.colorEnd = colorEnd;
+        }
+
+        public void reverseAnimation() {
+            float sunYStart = sunEnd;
+            float sunYEnd = sunStart;
+
+            float reflectionYStart = reflectionEnd;
+            float reflectionYEnd = reflectionStart;
+            Log.d("SUNCOORDS", "Sun to: " + sunYEnd);
+
+            AnimationTarget target = new AnimationTarget();
+
+            target.setSunStart(sunYStart);
+            target.setSunEnd(sunYEnd);
+            target.setReflectionStart(reflectionYStart);
+            target.setReflectionEnd(reflectionYEnd);
+            target.setColorStart(mNightSkyColor);
+            target.setColorEnd(mBlueSkyColor);
+
+            ObjectAnimator heightAnimator = ObjectAnimator
+                    .ofFloat(mSunView, "y", sunYStart, sunYEnd)
+                    .setDuration(ANIMATION_TIME);
+
+            ObjectAnimator sunSkyAnimator = ObjectAnimator
+                    .ofInt(mSkyView, "backgroundColor", mSunsetSkyColor, mBlueSkyColor)
+                    .setDuration(ANIMATION_TIME);
+
+            ObjectAnimator reflectionHeightAnimator = ObjectAnimator
+                    .ofFloat(mReflectionView, "y", reflectionYStart, reflectionYEnd)
+                    .setDuration(ANIMATION_TIME);
+
+            ObjectAnimator nightSkyAnimator = ObjectAnimator
+                    .ofInt(mSkyView, "backgroundColor", mNightSkyColor, mSunsetSkyColor)
+                    .setDuration(ANIMATION_TIME);
+
+
+            sunSkyAnimator.setEvaluator(new ArgbEvaluator());
+            nightSkyAnimator.setEvaluator(new ArgbEvaluator());
+            heightAnimator.setInterpolator(new AccelerateInterpolator());
+            reflectionHeightAnimator.setInterpolator(new AccelerateInterpolator());
+
+            mAnimatorSet = new AnimatorSet();
+            mAnimatorSet
+                    .play(heightAnimator)
+                    .with(reflectionHeightAnimator)
+                    .with(sunSkyAnimator)
+                    .after(nightSkyAnimator);
+            mAnimatorSet.start();
+
+            mAnimatorSet.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+                    isAnimating = true;
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                    Log.i("TEST", "Animation Finished");
+                    isSunSet = false;
+                    isAnimating = false;
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+                    mAnimatorSet = null;
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+
+                }
+            });
+        }
+
     }
 }
